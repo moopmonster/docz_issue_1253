349cb15834951eacd93b3689945ecae2a17d94bd
rakannimer <rakannimer@gmail.com>
2019-12-03 14:22:10 +0200

    fix webpack resolve modules

--- docs/gatsby-node.js
+++ docs/gatsby-node.js
@@ -1,10 +1,13 @@
 // gatsby-node.js
-const path = require('path')
+const path = require("path");
 
 exports.onCreateWebpackConfig = args => {
   args.actions.setWebpackConfig({
     resolve: {
-      modules: [path.resolve(__dirname, '../newtest/src/components'), 'node_modules']
-    },
-  })
-}
+      modules: [
+        path.resolve(__dirname, "../../newtest/src/components"),
+        "node_modules"
+      ]
+    }
+  });
+};
